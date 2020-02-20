a8e2216f5670d36f2bc0b9340e944ee2e6c41e08
JohanCederschiold <johan.cederschiold@gmail.com>
2020-02-20 06:36:36 +0100

    FirstCommit

---
+++ src/main/java/se/jpdc/restdemos/RestdemosApplication.java
@@ -0,0 +1,13 @@
+package se.jpdc.restdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/jpdc/restdemos/RestdemosApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jpdc.restdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
